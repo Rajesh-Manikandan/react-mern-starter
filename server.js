7d15bb4f3ab19fbe2dfac58c9f80b2230f06aad9
Rajesh <welearntogether.rj@gmail.com>
2020-08-09 22:38:37 +0530

    'update'

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 const express = require("express");
 const app = express();
 
+//Setting Port
 const port = 5000;
 
+//Sample endpoint
 app.get("/", (req, res) => {
   res.send("Response from API");
 });
 
+// Started Server to Listen Port
 app.listen(port, () => console.log("Server started on Port:" + port));
