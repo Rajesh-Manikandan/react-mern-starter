04f628eebd1f5a5644ecc9b52a01e62c87eabcc4
Rajesh <welearntogether.rj@gmail.com>
2020-08-09 22:10:29 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+//Instantiate App
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Response from API");
+});
+
+app.listen(port, () => console.log("Server started on Port:" + port));
